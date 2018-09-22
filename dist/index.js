9821f6a968f4111d7996166688e3504878451d87
michaelolof <myqrrlolof@yahoo.com>
2018-09-22 18:11:27 +0100

    changes made @ Sat, Sep 22, 2018  6:11:26 PM

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Promise.prototype.withErrors = function () {
     return this
-        .then(data => data)
-        .catch(err => new Error(err));
+        .then(data => [data, undefined])
+        .catch(err => [undefined, new Error(err)]);
 };
